d15a49a8d1541ec1e8ba2e370c71365a4c580293
Daniel Hossaki <daniel.hossaki@outlook.com>
2024-10-22 16:00:25 -0300

    [FEAT] Display post by ID

--- services/api.js
+++ services/api.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 export const api  = axios.create({
-    baseURL: 'https://nuareafrukmnjnaakplk.supabase.co/rest/v1',
+    baseURL: 'https://nuvnfakeqwndaotqcbgl.supabase.co/rest/v1',
     headers: {
-        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51YXJlYWZydWttbmpuYWFrcGxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAyMjc0MjEsImV4cCI6MTk3NTgwMzQyMX0.3qUEldVsmcr_yrpf8N1-qBGdHLB1QCy9nB6-nApVQIA",
-        authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51YXJlYWZydWttbmpuYWFrcGxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAyMjc0MjEsImV4cCI6MTk3NTgwMzQyMX0.3qUEldVsmcr_yrpf8N1-qBGdHLB1QCy9nB6-nApVQIA"
+        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51dm5mYWtlcXduZGFvdHFjYmdsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjk2MTQ4MTQsImV4cCI6MjA0NTE5MDgxNH0.OAMqsp56P1KdhaRxIms5y9U30rb8UuxFy7_0dqOp9fM",
+        authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51dm5mYWtlcXduZGFvdHFjYmdsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjk2MTQ4MTQsImV4cCI6MjA0NTE5MDgxNH0.OAMqsp56P1KdhaRxIms5y9U30rb8UuxFy7_0dqOp9fM"
     }
 })
